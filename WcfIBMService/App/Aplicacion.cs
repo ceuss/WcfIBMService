7d871c627e16ae95201bcd578f4eb6f3de80dde7
ceuss <ceuss89@gmail.com>
2019-03-07 17:48:23 +0100

    Declaración de métodos de la aplicación

---
+++ WcfIBMService/App/Aplicacion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WcfIBMService
+{
+    public class Aplicacion : IAplicacion
+    {
+        public Task<string> GetRates()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetTransactions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SkuDetails> GetTransactionsListOf(string sku)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WcfIBMService/Models/Rate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfIBMService
+{
+    public class Rate
+    {
+        public string from;
+        public string to;
+        public string rate;
+    }
+}
